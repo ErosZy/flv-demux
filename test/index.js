82c93dede1e172bf02e91aebbb23d17fb7176148
zyEros <zyeros1991@gmail.com>
2017-09-18 17:02:35 +0800

    add test detail

--- test/index.js
+++ test/index.js
@@ -9,13 +9,28 @@ function start() {
   let buf = Buffer.alloc(stat.size);
   fs.readSync(fd, buf, 0, stat.size);
 
-  console.log('whole data parse start....');
+  console.log('[I] whole data parse start');
 
   let startTime = +new Date();
   let decoder = new FlvDemux.Decoder();
+
+  decoder.on('header', header => {
+    console.log(
+      `[I] parse header success, hasAudio:${header.hasAudio}, hasVideo:${header.hasVideo}`
+    );
+  });
+
+  decoder.on('tag', tag => {
+    if (tag.type == FlvDemux.VideoTag.TYPE) {
+      if (tag.data.AVCPacketType == 2) {
+        console.log('[I] parse end AVC chunk success');
+      }
+    }
+  });
+
   decoder.decode(buf);
 
-  console.log(`time consuming: ${+new Date() - startTime}ms`);
+  console.log(`[I] time consuming: ${+new Date() - startTime}ms`);
 
   parseChunkData();
 }
@@ -27,16 +42,30 @@ function parseChunkData() {
   let buf = Buffer.alloc(halfSize);
   fs.readSync(fd, buf, 0, halfSize);
 
-  console.log('chunk data parse start....');
+  console.log('--------------------------------');
+  console.log('[I] chunk data parse start....');
 
   let startTime = +new Date();
   let decoder = new FlvDemux.Decoder();
+  decoder.on('header', header => {
+    console.log(
+      `[I] parse header success, hasAudio:${header.hasAudio}, hasVideo:${header.hasVideo}`
+    );
+  });
+
+  decoder.on('tag', tag => {
+    if (tag.type == FlvDemux.VideoTag.TYPE) {
+      if (tag.data.AVCPacketType == 2) {
+        console.log('[I] parse end AVC chunk success');
+      }
+    }
+  });
   decoder.decode(buf);
 
   setTimeout(() => {
     let buf = Buffer.alloc(stat.size - halfSize);
     fs.readSync(fd, buf, 0, stat.size - halfSize, halfSize);
     decoder.decode(buf);
-    console.log('ALL TEST PASSED');
+    console.log('[I[ ALL TEST PASSED');
   }, 5000);
 }
